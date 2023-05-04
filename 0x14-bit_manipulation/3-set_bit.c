acfdd517a3232ab8e87b251b0b5832899b26319a
nissrine <nissrinetabli@gmail.com>
2023-05-04 16:30:13 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,10 +8,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
-	{
-	return (-1);
-	}
-	*n = (*n & ~(1UL << index)) | (1UL << index);
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
+	*n |= (1 << index);
+
 	return (1);
 }
