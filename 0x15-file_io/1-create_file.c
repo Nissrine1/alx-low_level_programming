55683866b67bdef8f5d2d56e5113373e8e0d5bdc
nissrine <nissrinetabli@gmail.com>
2023-05-08 16:15:32 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,41 @@
 #include "main.h"
 
 /**
- * create_file - creates a new file and writes text content to it
- * @filename: name of the file to create
- * @text_content: content to write to the file
- *
- * Return: 1 on success, -1 on failure
- */
+* create_file - creates a file.
+*
+* @filename: a pointer to the file name
+* @text_content: string to write to the file
+*
+* Return: returns 1 on success, -1 on failure
+*/
+
 int create_file(const char *filename, char *text_content)
 {
-		int file_desc, n_letters, result;
+	int fd;
+	ssize_t num_written;
 
-		if (!filename)
+	if (filename == NULL)
+	{
 		return (-1);
-
-		file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-		if (file_desc == -1)
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
 		return (-1);
-
-		if (!text_content)
-		text_content = "";
-
-		for (n_letters = 0; text_content[n_letters]; n_letters++);
-
-		result = write(file_desc, text_content, n_letters);
-
-		if (result == -1)
+	}
+	if (text_content != NULL)
+	{
+		num_written = write(fd, text_content, strlen(text_content));
+	}
+	else
+	{
+		num_written = 0;
+	}
+	if (num_written == -1)
+	{
+		close(fd);
 		return (-1);
-
-		close(file_desc);
-
-		return (1);
+	}
+	close(fd);
+	return (1);
 }
